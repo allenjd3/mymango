dc69725c082af8e81846c6b73647e062c1804ffe
James Allen <jacques2186@yahoo.com>
2024-02-03 14:45:16 -0500

    Add pw to credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(
+  email: 'jacques2186@yahoo.com',
+  password: Rails.application.credentials.main_user_pw
+)
